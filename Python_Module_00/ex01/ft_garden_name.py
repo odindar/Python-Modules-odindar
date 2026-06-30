75cdb8bc31de4ed826960005890f6f4370186ab9
Slyord <dindaromerrecep@gmail.com>
2026-06-30 15:08:42 +0300

    module00 - ex02 - added

--- Python_Module_00/ex01/ft_garden_name.py
+++ Python_Module_00/ex01/ft_garden_name.py
@@ -1,4 +1,5 @@
 def ft_garden_name() -> None:
     garden: str = input("Enter garden name: ")
+
     print(f"Garden: {garden}")
     print("Status: Growing well!")
---
+++ Python_Module_00/ex02/ft_plot_area.py
@@ -0,0 +1,7 @@
+def ft_plot_area() -> None:
+    leng: int = int(input("Enter length: "))
+    widt: int = int(input("Enter width: "))
+
+    total: int = leng * widt
+
+    print(f"Plot area: {total}")
