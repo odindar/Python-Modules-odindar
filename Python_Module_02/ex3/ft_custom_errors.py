fa7926f1f59f5f3029238711e5e40cc47c7633b4
odindar <dindaromerrecep@gmail.com>
2026-08-06 23:06:37 +0300

    py02ex3 added

---
+++ Python_Module_02/ex3/ft_custom_errors.py
@@ -0,0 +1,49 @@
+
+class GardenError(Exception):
+    def __init__(self, message: str = "Unknown garden error"):
+        super().__init__(message)
+
+class PlantError(GardenError):
+    def __init__(self, message: str = "Unknown plant error"):
+        super().__init__(message)
+
+class WaterError(GardenError):
+    def __init__(self, message: str = "Unknown water error"):
+        super().__init__(message)
+
+def raise_plant_error() -> None:
+    raise PlantError("The tomato plant is wilting!")
+
+def raise_water_error() -> None:
+    raise WaterError("Not enough water in the tank!")
+
+def test_custom_errors() -> None:
+    print("=== Custom Garden Errors Demo ===\n")
+    
+    print("Testing PlantError...")
+    try:
+        raise_plant_error()
+    except PlantError as e:
+        print(f"Caught PlantError: {e}\n")
+        
+    print("Testing WaterError...")
+    try:
+        raise_water_error()
+    except WaterError as e:
+        print(f"Caught WaterError: {e}\n")
+        
+    print("Testing catching all garden errors..")
+    try:
+        raise_plant_error()
+    except GardenError as e:
+        print(f"Caught GardenError: {e}")
+        
+    try:
+        raise_water_error()
+    except GardenError as e:
+        print(f"Caught GardenError: {e}")
+        
+    print("\nAll custom error types work correctly!")
+
+if __name__ == "__main__":
+    test_custom_errors()
